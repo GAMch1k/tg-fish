6ab646ce3b114bf18c304a632bfb29286311f5c5
GAMch1k <gammail404@gmail.com>
2024-05-21 20:45:02 +0200

    Frontend added and connected

--- cmd/back-end/internal/server/server.go
+++ cmd/back-end/internal/server/server.go
@@ -5,11 +5,17 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func Run(port string) {
 	app := fiber.New()
 
+	app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowMethods: "GET,POST,DELETE",
+        AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+    }))
 
 	app.Post("/phone", PostPhone)
 	app.Post("/code", PostCode)
--- cmd/front-end/internal/server/server.go
+++ cmd/front-end/internal/server/server.go
@@ -1,18 +1,30 @@
 package server
 
 import (
-	"log"
 	"fmt"
+	"log"
+	"path"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func Run(port string) {
 	app := fiber.New()
 
-	app.Get("/", func(c fiber.Ctx) error {
-		return c.SendString("Front End")
-	})
+	app.Use(cors.New(cors.Config{
+        AllowOrigins: "*",
+        AllowMethods: "GET,POST,DELETE",
+        AllowHeaders: "Origin,Content-Type,Accept,Content-Length,Accept-Language,Accept-Encoding,Connection,Access-Control-Allow-Origin",
+    }))
+
+	app.Static("/", path.Join(
+		"cmd", "front-end", "internal",
+		"server", "static",
+	))
+	// app.Get("/", func(c fiber.Ctx) error {
+	// 	return c.SendString("Front End")
+	// })
 
 
 	log.Printf("Front End server: http://localhost:%s", port)
